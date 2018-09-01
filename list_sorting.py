3b4e76b866d164541683208951b26c6a246befb9
janetnamutebi460@gmail.com <janetnamutebi460@gmail.com>
2018-09-01 19:44:02 +0300

    moved sorting and updating out of the for loop

--- list_sorting.py
+++ list_sorting.py
@@ -4,31 +4,36 @@ def list_sort(a):
         #list categories
         word_cat = ['evens','odds','chars']
         #sorted lis
-        sorted_list = {}
+        sorted_dictionary = {}
         char_list = []
         evens_list = [] 
         odds_list = []
         for x in a:
             if isinstance(x, str):
                 char_list.append(x)
-                sorted_list[word_cat[2]] = char_list
-                char_list.sort()
+                
             elif x % 2 == 1:
                 evens_list.append(x)
-                sorted_list[word_cat[1]] = evens_list
-                evens_list.sort()
+                
             elif x % 2 == 0:
                 odds_list.append(x)
-                sorted_list[word_cat[0]] = odds_list
-                odds_list.sort()
             else:
                 return False
+
+        sorted_dictionary[word_cat[2]] = char_list
+        char_list.sort()
+        sorted_dictionary[word_cat[1]] = evens_list
+        evens_list.sort()
+        sorted_dictionary[word_cat[0]] = odds_list
+        odds_list.sort()
 
     else:
         return "Not a List"
         
-    return sorted_list
+    return sorted_dictionary
 
 
 #call function for list 
 print (list_sort([2,0,6,5,1,7,'z','a']))
+print (list_sort([4,9,7,8,'d','g','!']))
+print (list_sort([]))
