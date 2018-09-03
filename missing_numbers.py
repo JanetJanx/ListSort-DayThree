f078c08210d4763c006f00caca1f27ec9e5b70b6
janetnamutebi460@gmail.com <janetnamutebi460@gmail.com>
2018-09-03 13:46:40 +0300

    create list with out a loop removing unnecessary else and modifying my if condition

--- missing_numbers.py
+++ missing_numbers.py
@@ -1,14 +1,14 @@
 def missing_number(b):
-    if len(b) != 0:
-        first_list = []
-        for x in range(b[0], b[-1]+1):
-            first_list.append(x)
+    if b:
+        first_list = [*range(b[0], b[-1]+1)]
+        
         new_b = set(b)
         
         return (list(new_b ^ set(first_list)))
-    else:
-        return "Empty List"
+    
+    return "Empty List"
 
+print (missing_number([10,14,20]))
 print (missing_number([]))
    
 
