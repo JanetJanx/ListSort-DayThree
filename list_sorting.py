954a1167c4f9b0b83df2289699c030c4e48a0298
janetnamutebi460@gmail.com <janetnamutebi460@gmail.com>
2018-09-05 17:57:18 +0300

    add comments

--- list_sorting.py
+++ list_sorting.py
@@ -1,20 +1,20 @@
 #function to sort elements according to evens, odds and characters
 def list_sort(a):
+    #a is the provided list
     if isinstance(a, list):
         #list categories
         word_cat = ['evens','odds','chars']
-        #sorted lis
+        #sorted dictionary
         sorted_dictionary = {}
+        #categorized lists
         char_list = []
         evens_list = [] 
         odds_list = []
         for x in a:
             if isinstance(x, str):
-                char_list.append(x)
-                
+                char_list.append(x)    
             elif x % 2 == 1:
-                evens_list.append(x)
-                
+                evens_list.append(x)  
             elif x % 2 == 0:
                 odds_list.append(x)
             else:
@@ -35,5 +35,5 @@ def list_sort(a):
 
 #call function for list 
 print (list_sort([2,0,6,5,1,7,'z','a']))
-print (list_sort([4,9,7,8,'d','g','!']))
+print (list_sort([4,9,7,8,False,True,'d','g','!']))
 print (list_sort([]))
