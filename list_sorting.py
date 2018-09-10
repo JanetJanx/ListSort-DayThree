ffe6b619d8a0a91bbb062aa143c9bbd329198427
janetnamutebi460@gmail.com <janetnamutebi460@gmail.com>
2018-09-10 17:10:45 +0300

    change on type checking and avoid nestings

--- list_sorting.py
+++ list_sorting.py
@@ -1,35 +1,31 @@
 #function to sort elements according to evens, odds and characters
 def list_sort(a):
     #a is the provided list
-    if isinstance(a, list):
-        #list categories
-        word_cat = ['evens','odds','chars']
-        #sorted dictionary
-        sorted_dictionary = {}
-        #categorized lists
-        char_list = []
-        evens_list = [] 
-        odds_list = []
-
-        for x in a:
-            if isinstance(x, str) and x is not False and x is not True:
-                char_list.append(x)    
-            elif x % 2 == 1 and x is not False and x is not True:
-                evens_list.append(x)  
-            elif x % 2 == 0 and x is not False and x is not True:
-                odds_list.append(x)
-            else:
-                return "List contains boolean values"
-
-        sorted_dictionary[word_cat[2]] = char_list
-        char_list.sort()
-        sorted_dictionary[word_cat[1]] = evens_list
-        evens_list.sort()
-        sorted_dictionary[word_cat[0]] = odds_list
-        odds_list.sort()
-
-    else:
+    if not isinstance(a, list):
         return "Not a List"
+    #list categories
+    word_cat = ['evens','odds','chars']
+    #sorted dictionary
+    sorted_dictionary = {}
+    #categorized lists
+    char_list = []
+    evens_list = [] 
+    odds_list = []
+
+    for x in a:
+        if isinstance(x, str):
+            char_list.append(x)    
+        elif x % 2 == 1 and type(x) == int:
+            evens_list.append(x)  
+        elif x % 2 == 0 and type(x) == int:
+            odds_list.append(x)
+
+    sorted_dictionary[word_cat[2]] = sorted(char_list)
+    
+    sorted_dictionary[word_cat[1]] = sorted(evens_list)
+    
+    sorted_dictionary[word_cat[0]] = sorted(odds_list)
+
         
     return sorted_dictionary
 
